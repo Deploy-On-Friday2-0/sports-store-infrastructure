9da59833137016972670a94122cae978dbf1609f
omerl01 <01omerlevi@gmail.com>
2026-07-25 22:21:47 +0300

    feat(infra): Multi-AZ AWS VPC Networking Setup (DEP-189)

---
+++ providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.5.0, < 2.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  cloud {
+    organization = "deploy-on-friday"
+    workspaces {
+      name = "sports-store-infrastructure"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
---
+++ vpc.tf
@@ -0,0 +1,43 @@
+locals {
+  is_prod = lower(var.environment) == "prod"
+
+  single_nat_gateway     = local.is_prod ? false : true
+  one_nat_gateway_per_az = local.is_prod ? true : false
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  # Egress routing details
+  enable_nat_gateway     = true
+  single_nat_gateway     = local.single_nat_gateway
+  one_nat_gateway_per_az = local.one_nat_gateway_per_az
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  # Load Balancer Discovery Tags
+  public_subnet_tags = {
+    "kubernetes.io/role/elb"                    = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb"           = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+
+  tags = {
+    Environment = var.environment
+    Terraform   = "true"
+  }
+}
+
